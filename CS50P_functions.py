7143d316aa4eb65484499cee3580547c24d83432
Tweetsauce <pobusauce12@gmail.com>
2023-08-03 20:47:52 +0400

    more exercises

---
+++ CS50P_functions.py
@@ -0,0 +1,14 @@
+# default params just put ="param" BROOO WHAT IS PYTHON
+# in python, functions must exist before you call em
+# u wanna be putting the main part of your code at the top of your file??? 
+def main():
+    hello()
+    name = input("what name? ")
+    hello(name)
+
+
+def hello(to="world"):
+    print("hello,", to)
+
+# call main at da end i see i see
+main()
--- cs50p_calculator.py
+++ cs50p_calculator.py
@@ -6,8 +6,19 @@
 # this does idek atp bruh (z:,)
 # oh it like adds the comma after/before? 3 zeros like idk some formatting stuff
 
-x = float(input("What's x? "))
-y = float(input("What's y? "))
+# x = float(input("What's x? "))
+# y = float(input("What's y? "))
 
-z = round(x / y, 2)
-print(z)
+# z = round(x / y, 2)
+# print(z)
+
+def main():
+    x = int(input("What's x? "))
+    print("x squared is", square(x))
+
+# skipped ahead my solution:
+def square(num):
+    return num * num
+
+# wow i was right im a god
+main()
