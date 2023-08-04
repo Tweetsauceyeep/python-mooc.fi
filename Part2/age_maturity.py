9c5dfdf69e537b80c12699ca7a2925711a05ecd4
Tweetsauce <pobusauce12@gmail.com>
2023-08-04 21:39:31 +0400

    age maturity

---
+++ Part2/age_maturity.py
@@ -0,0 +1,7 @@
+age = int(input("age"))
+
+if age > 18: 
+    print("You are of age!")
+
+else:
+    print("You are not of age")
---
+++ Part2/type_casting.py
@@ -0,0 +1,7 @@
+number = float(input("give float now"))
+
+integer = int(number)
+decimal = float(number - integer)
+
+print(f"Integer part: {integer}")
+print(f"Decimal part: {decimal}")
