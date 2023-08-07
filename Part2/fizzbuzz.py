23d25368bc33fb986a1a64469fd2b10abadde486
Tweetsauce <pobusauce12@gmail.com>
2023-08-07 14:34:20 +0400

    more exercises

---
+++ Part2/fizzbuzz.py
@@ -0,0 +1,10 @@
+# fizzybussy
+
+num = int(input("input num plz: "))
+
+if num % 5 == 0 and num % 3 == 0:
+    print("FizzBuzz")
+elif num % 3 == 0:
+    print("Fizz")
+elif num % 5 == 0:
+    print("Buzz")
